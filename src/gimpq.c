9c4c52c965b6916d1bc77eef20ce35bd9cd12c75
Matt Wittmann <mcwitt@gmail.com>
2013-05-16 15:19:45 -0700

    Finished rough version of line minimization routine.

--- src/gimpq.c
+++ src/gimpq.c
@@ -50,6 +50,60 @@ void gq_init(GQProblem *p, int g[N][N], double h[N])
     }
 }
 
+
+double gq_driver_matrix_element(double u[D], double v[D])
+{
+    double result = 0.;
+    ULONG i;
+    int j;
+
+    for (i = 0; i < D; i++)
+        for (j = 0; j < N; j++)
+            result += u[i] * v[NEIGHBOR(i, j)];
+}
+
+void gq_problem_matrix_element(double d[D], double u[D], double v[D], double *udotv)
+{
+    double udotv_i, result = 0.;
+    ULONG i;
+
+    *udotv = 0.;
+
+    for (i = 0; i < D; i++)
+    {
+        udotv_i = u[i] * v[i];
+        *udotv += udotv_i;
+        result += d[i] * udotv_i;
+    }
+
+    return result;
+}
+
+double gq_matrix_element(double s, double d[D], double u[D], double v[D], double *udotv)
+{
+    return (1. - s) * gq_driver_matrix_element(u, v)
+               + s  * gq_problem_matrix_element(d, u, v, udotv);
+}
+
+void gq_grad_energy(double s, double d[D], double psi[D], double grad[D])
+{
+    int k;
+    double psi2, energy;
+
+    /* compute the energy */
+    energy = gq_matrix_element(s, d, psi, psi, &psi2);
+    energy /= psi2;
+
+    /* compute the gradient */
+    for (k = 0; k < D; k++)
+    {
+        double apsi_i = 0.;   /* component of A psi */
+
+        for (j = 0; j < N; j++) apsi_i += psi[NEIGHBOR(k, j)];
+        grad[k] = 2. * (psi[k] * (s * p->d[k] - energy) + (1. - s) * apsi_i) / psi2;
+    }
+}
+
 void gq_grad_energy(GQProblem *p, double s, double c[D], double grad[D])
 {
     ULONG i;
@@ -86,23 +140,17 @@ void gq_grad_energy(GQProblem *p, double s, double c[D], double grad[D])
 
 double gq_line_min(GQProblem *p, double s, double psi[D], double delta[D])
 {
-    double psi_dot_delta = 0.,
-           psi2 = 0.,
-           delta2 = 0.,
-           psi_H_delta = 0.,
-           psi_H_psi = 0.,
-           delta_H_delta = 0.;
-    int i;
+    double psi2, psi_dot_delta, delta2, psi_H_psi, psi_H_delta, delta_H_delta,
+           alpha, beta, gamma;
 
-    for (i = 0; i < D; i++)
-    {
-        psi2 += 
-        psi_dot_delta += psi[i] * delta[i];
-    }
+    psi_H_psi = gq_matrix_element(s, d, psi, psi, &psi2);
+    psi_H_delta = gq_matrix_element(s, d, psi, delta, &psi_dot_delta);
+    delta_H_delta = gq_matrix_element(s, d, delta, delta, &delta2);
 
+    alpha = psi_dot_delta * delta_H_delta - delta2 * psi_H_delta;
+    beta  = psi2 * delta_H_delta - delta2 * psi_H_psi;
+    gamma = psi2 * delta_H_psi - psi_dot_delta * psi_H_psi;
 
-
-
-    
-
+    disc = beta*beta - 4.*alpha*gamma;
+    return 0.5*(sqrt(disc) - beta) / alpha;
 }
