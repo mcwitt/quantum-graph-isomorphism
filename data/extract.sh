3bbff89272abea1e5e26fe0a8bfd0403d2bd2153
Matt Wittmann <mcwitt@gmail.com>
2013-07-16 12:52:13 -0700

    Modified script.

--- data/extract.sh
+++ data/extract.sh
@@ -2,5 +2,5 @@
 
 for f in $@; do
     grep '^[01]*$' $f > tmp && mv tmp $f
-    awk -v bn=${f%.*} -v ex=${f##*.} '{print $0 > bn "-" NR "." ex}' RS='\n\n' $f
+    awk '{print $0 > FILENAME "-" NR}' RS='\n\n' $f
 done
