9fee9c2cf4f4b96e7adb983d9ad295fd75015c82
Matt Wittmann <mcwitt@gmail.com>
2013-07-25 11:56:57 -0700

    Added analysis notebooks.

--- analysis/gimp.pyx
+++ analysis/gimp.pyx
@@ -3,24 +3,33 @@ import scipy.sparse
 from libc.stdint cimport uint32_t
 cimport numpy as np
 
+def load_graph(filename):
+    f = open(filename, 'r')
+    a = []
+    for line in f: a.append(list(line.strip()))
+    a = np.array(a, dtype=int)
+    assert(a.shape[0] == a.shape[1])
+    return a
+
 cdef inline int spin(uint32_t i, int j):
     return (((1 << j) & i) >> j)*2 - 1
 
 cdef inline uint32_t neighbor(uint32_t i, int j):
     return (1 << j) ^ i
 
-def hamiltonian5(np.ndarray[long, ndim=2] a,
-                 np.ndarray[double] h,
-                 double s):
+def hamiltonian(np.ndarray[long, ndim=2] a,
+                np.ndarray[double] h,
+                double s):
     
     N = len(a)
     d = 2**N
-    nnz = d*N*(N+3)/2 # number of nonzero entries
+    nnz = d*N*(N+3)/2   # number of nonzero entries
     
     cdef np.ndarray[int]    rows = np.zeros(nnz, dtype=np.intc)
     cdef np.ndarray[int]    cols = np.zeros(nnz, dtype=np.intc)
     cdef np.ndarray[double] vals = np.zeros(nnz, dtype=np.double)
-    cdef int m, n, s_n
+    cdef double c
+    cdef int m, n
     cdef uint32_t i, inz = 0
 
     # transverse field
@@ -42,12 +51,12 @@ def hamiltonian5(np.ndarray[long, ndim=2] a,
     #  antiferromagnetic exchange
     for i in range(d):
         for n in range(N):
-            s_n = spin(i, n)
+            c = s * spin(i, n)
             for m in range(n):
                 if a[n, m] == 1:
                     rows[inz] = i
                     cols[inz] = i
-                    vals[inz] = s * s_n * spin(i, m)
+                    vals[inz] = c * spin(i, m)
                     inz += 1
 
     return scipy.sparse.coo_matrix((vals, (rows, cols)), shape=(d, d))
