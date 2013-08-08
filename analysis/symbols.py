b48ad07e3e211d6aece0fc85e475cf7e7d530cab
Matt Wittmann <mcwitt@gmail.com>
2013-08-08 15:09:55 -0700

    Added symbols.

--- analysis/symbols.py
+++ analysis/symbols.py
@@ -1,6 +1,8 @@
 symbols = {'energy': 'E_0',
            'mz'    : 'm_z',
            'mx'    : 'm_x',
-           'q2'    : 'Q_2'}
+           'q2'    : 'Q_2',
+           'res2'  : 'r^2',
+           'iterations': r'\mathrm{iterations}'}
 
 def symbol(s): return symbols[s] if s in symbols else s
