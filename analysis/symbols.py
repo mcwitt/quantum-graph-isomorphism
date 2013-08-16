b069b9eb5e390b22809151ad863d8c975135f9f6
Matt Wittmann <mcwitt@gmail.com>
2013-08-16 15:40:47 -0700

    Update analysis.

--- analysis/symbols.py
+++ analysis/symbols.py
@@ -2,6 +2,7 @@ symbols = {'energy': 'E_0',
            'mz'    : 'm_z',
            'mx'    : 'm_x',
            'q2'    : 'Q_2',
+           'q2p'   : 'Q_2^{\prime}',
            'res2'  : 'r^2',
            'iterations': r'\mathrm{iterations}'}
 
