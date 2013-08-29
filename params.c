177546ed6af894d32094e7f6e69aecdf19195281
Matt Wittmann <mcwitt@gmail.com>
2013-08-29 08:59:49 -0600

    Fix bug in linspace n=1.

--- params.c
+++ params.c
@@ -26,9 +26,10 @@ static void print_usage(int argc, char *argv[])
 
 static double *linspace(double left, double right, int n)
 {
-    double *a, delta = (right - left) / (n - 1);
+    double *a, delta;
     int i;
 
+    delta = (n > 1) ? (right - left) / (n - 1) : 0.;
     a = (double*) malloc(n * sizeof(double));
     for (i = 0; i < n; i++) a[i] = left + i*delta;
     return a;
